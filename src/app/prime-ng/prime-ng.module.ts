06eba6449a9322ad921b211af566ff55693a7d39
Nicolas Inostroza <ninostrozacordova2016@gmail.com>
2023-12-26 17:44:45 -0300

    pipes terminados con fernando herrera

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
-import { MenuModule } from "primeng/menu"
 import { ButtonModule } from "primeng/button"
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 
 @NgModule({
   exports: [
-    // MenuModule,
     ButtonModule,
     MenubarModule,
     CardModule,
     FieldsetModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule,
+    TableModule
   ]
 })
 export class PrimeNgModule { }
